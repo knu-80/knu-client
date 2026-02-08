42377c9f096e5c6be9a89b150bcadf0d4e0da49a
Junsub Lee <lee980605@knu.ac.kr>
2026-02-08 23:04:38 +0900

    [#9]Feat: 라우팅 구조 적용 - MainLayout 아래에 HomePage 라우트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+
+import MainLayout from './components/layouts/MainLayout';
 import HomePage from './pages/HomePage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;

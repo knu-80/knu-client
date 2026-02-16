b98587c154b4bb397bae1129cf4dab3829768c75
Seah Kim <seahkeem@gmail.com>
2026-02-16 21:37:29 +0900

    Style: 지도 배경 추가

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { SearchBar } from '@/components/SearchBar';
 import { MapPageClubCategory } from '@/components/ClubCategory';
+import MapSvg from '@/assets/map.svg';
 
 export default function MapPage() {
   const [value, setValue] = useState('');
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="py-3 bg-white z-10">
+    <div className="h-full flex flex-col overflow-hidden">
+      <div className="px-5 py-3 z-30">
         <SearchBar
           value={value}
           onChange={setValue}
@@ -16,13 +17,19 @@ export default function MapPage() {
         />
       </div>
 
-      <div className="flex gap-[10px] whitespace-nowrap">
-        {['문예부', '사회부', '학술부', '체육부', '종교부'].map(d => (
-          <MapPageClubCategory key={d} divisionName={d} />
-        ))}
-      </div>
-
-      <div className="flex-1 bg-gray-50">
+      <div className="relative flex-1 overflow-auto bg-gray-50">
+        <div className="relative w-[700px] h-[3000px] mx-auto">
+          <img
+            src={MapSvg}
+            alt="지도"
+            className="absolute top-0 left-0 w-full h-full"
+          />
+          <div className="sticky top-0 z-20 flex gap-[10px] overflow-x-auto px-5 py-3">
+            {['문예부', '사회부', '학술부', '체육부', '종교부'].map(d => (
+              <MapPageClubCategory key={d} divisionName={d} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );

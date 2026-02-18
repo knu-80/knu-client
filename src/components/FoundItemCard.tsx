3057238502cc2cce80945dbea6374abccc62e73e
seongmin <sm4046@naver.com>
2026-02-19 01:25:25 +0900

    [#18]Design: 분실물 정보 컴포넌트 구현

---
+++ src/components/FoundItemCard.tsx
@@ -0,0 +1,30 @@
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { BsBox2Fill } from 'react-icons/bs';
+
+interface FoundItemCardProps {
+  itemName: string;
+  foundLocation: string;
+}
+
+export default function FoundItemCard({ itemName, foundLocation }: FoundItemCardProps) {
+  return (
+    <div className="flex items-center justify-between p-4 border border-gray-200 rounded-lg shadow-sm mb-3 bg-white">
+      <div className="flex items-center space-x-3 w-1/2 pr-2">
+        <BsBox2Fill className="text-knu-gold text-2xl" />
+        <div className="flex flex-col">
+          <span className="text-xs text-gray-500 font-medium">습득 물품</span>
+          <span className="text-base text-black font-semibold wrap-break-word">{itemName}</span>
+        </div>
+      </div>
+      <div className="flex items-center space-x-3 w-1/2 pl-2">
+        <FaMapMarkerAlt className="text-knu-red text-2xl" />
+        <div className="flex flex-col">
+          <span className="text-xs text-gray-500 font-medium">습득 장소</span>
+          <span className="text-base text-black font-semibold wrap-break-word">
+            {foundLocation}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}

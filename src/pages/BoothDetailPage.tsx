b9b851f208a111d9ed5bd6921f9a66a6a1ade139
seongmin <sm4046@naver.com>
2026-02-21 12:25:59 +0900

    [#7]Design: 부스 상세 페이지 상단 여백 수정

--- src/pages/BoothDetailPage.tsx
+++ src/pages/BoothDetailPage.tsx
@@ -7,7 +7,7 @@ import ClubCategory from '@/components/ClubCategory';
 
 export default function BoothDetailPage() {
   return (
-    <div className="pt-3 sm:p-5">
+    <div className="pt-5 sm:p-5">
       <div className="flex items-center space-x-2 mb-4">
         <BiMessageSquareDetail className="h-6 w-6 text-black" />
         <h2 className="typo-heading-2 text-black">상세보기</h2>

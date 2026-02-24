ec8c1a3375f5769c74ee53d9ee5faa701bbcb680
Junsub Lee <lee980605@knu.ac.kr>
2026-02-24 23:51:21 +0900

    [#24]Design: 홈 배너 색상을 KNU 팔레트로 통일 - 배너 배경과 오버레이를 knu-lavender 기반으로 변경

--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -4,7 +4,7 @@ export default function HomeBanner() {
   return (
     <section
       aria-labelledby="home-banner-title"
-      className="-mx-5 relative overflow-hidden bg-[#b89be2] select-none"
+      className="-mx-5 relative overflow-hidden bg-knu-lavender select-none"
     >
       <div
         className="relative h-[230px] w-full bg-cover bg-center bg-no-repeat sm:h-[290px]"
@@ -12,7 +12,7 @@ export default function HomeBanner() {
         role="img"
         aria-label="2026 경북대학교 가두모집 안내 포스터"
       >
-        <div className="absolute inset-0 bg-gradient-to-t from-[#b89be2]/45 via-transparent to-[#b89be2]/10" />
+        <div className="absolute inset-0 bg-gradient-to-t from-knu-lavender/45 via-transparent to-knu-lavender/10" />
       </div>
 
       <div className="pointer-events-none absolute -bottom-px left-0 right-0 h-8 rounded-t-[28px] bg-white" />

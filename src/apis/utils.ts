4d0af6154614df998fefecaa7f6718cd4e1994a1
Junsub Lee <lee980605@knu.ac.kr>
2026-03-02 21:56:56 +0900

    [#39]Comment: omitUndefined 유틸 주석 정책 정리 - undefined 제거 기본 정책과 null 사용 조건을 코드 주석에 반영

--- src/apis/utils.ts
+++ src/apis/utils.ts
@@ -1,4 +1,6 @@
 export function omitUndefined<T extends Record<string, unknown>>(value: T): Partial<T> {
+  // PATCH 기본 정책: undefined는 "변경 없음"으로 제거한다.
+  // null은 백엔드가 명시적으로 허용한 필드에서만 사용한다.
   const entries = Object.entries(value).filter(([, entryValue]) => entryValue !== undefined);
 
   return Object.fromEntries(entries) as Partial<T>;

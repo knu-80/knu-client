2ea7444cbf420ebfb9680634c65b03bf100cbc9d
Seah Kim <seahkeem@gmail.com>
2026-03-05 21:12:29 +0900

    [#49]Chore: Sentry 핸들러 개발/운영 환경 분기 처리

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,15 @@ import './index.css';
 import App from './App.tsx';
 import * as Sentry from '@sentry/react';
 
+const isProd = import.meta.env.MODE === 'production';
+
 const container = document.getElementById('root');
 const root = createRoot(container!, {
-  onUncaughtError: Sentry.reactErrorHandler(),
-  onCaughtError: Sentry.reactErrorHandler(),
-  onRecoverableError: Sentry.reactErrorHandler(),
+  ...(isProd && {
+    onUncaughtError: Sentry.reactErrorHandler(),
+    onCaughtError: Sentry.reactErrorHandler(),
+    onRecoverableError: Sentry.reactErrorHandler(),
+  }),
 });
 
 root.render(

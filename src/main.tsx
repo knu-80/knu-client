3fbd584f7594f2c99b966a59af7dce4791cfd749
Seah Kim <seahkeem@gmail.com>
2026-03-05 20:44:18 +0900

    [#49]Chore: Sentry 초기화, main.tsx 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import '../instrument.ts';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
+import * as Sentry from '@sentry/react';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+const root = createRoot(container!, {
+  onUncaughtError: Sentry.reactErrorHandler(),
+  onCaughtError: Sentry.reactErrorHandler(),
+  onRecoverableError: Sentry.reactErrorHandler(),
+});
+
+root.render(
   <StrictMode>
     <BrowserRouter>
       <App />

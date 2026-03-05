3145d48748a3b77c0629183f08f65b05cd0244d2
Seah Kim <seahkeem@gmail.com>
2026-03-05 21:12:29 +0900

    [#49]Chore: Sentry 핸들러 개발/운영 환경 분기 처리

--- instrument.ts
+++ instrument.ts
@@ -1,10 +1,13 @@
 import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  sendDefaultPii: true,
-  integrations: [Sentry.browserTracingIntegration()],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ['localhost'],
-  // 운영 서버 추가해야함
-});
+const isProd = import.meta.env.MODE === 'production';
+
+if (isProd) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    sendDefaultPii: true,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ['localhost', import.meta.env.VITE_API_BASE_URL],
+  });
+}
